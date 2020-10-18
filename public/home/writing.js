ee7ad0866f4ab0a0ef5e3382a47a2dd974ce99e3
Julia-Susser <you@example.com>
2020-10-18 16:03:50 -0700

    make it better

--- public/home/writing.js
+++ public/home/writing.js
@@ -23,7 +23,11 @@ txt.innerHTML = "Programmer"
 document.getElementById("writing").appendChild(txt)
 
 var txt = document.createElement("p")
-txt.innerHTML = "Assistant to University of Chicago Research Specialist <a href=https://bfi.uchicago.edu/scholar/alex-weinberg/ target='_blank'>Alex Weinberg</a>"
+txt.innerHTML = "Assistant to University of Chicago Economic Researcher Alex Weingberg"
+document.getElementById("writing").appendChild(txt)
+
+var txt = document.createElement("p")
+txt.innerHTML = "Good Samaritan Family Resource Center Node.js website and React-Native app developer."
 document.getElementById("writing").appendChild(txt)
 
 var txt = document.createElement("p")
@@ -39,53 +43,69 @@ txt.innerHTML = "Skills:"
 document.getElementById("border").appendChild(txt)
 
 
-var txt = document.createElement("dl")
-txt.id = "listy"
+
+var txt = document.createElement("p")
+txt.id = "sk"
+txt.innerHTML = "Python"
 document.getElementById("border").appendChild(txt)
 
-var txt = document.createElement("dt")
-var p = document.createElement("p")
-p.id="big"
-p.innerHTML="Python"
-txt.appendChild(p)
-document.getElementById("listy").appendChild(txt)
+var txt = document.createElement("UL")
+txt.id = "MyList"
+txt.innerHTML = `
+<li>Pandas</li>
+<li>SQL</li>
+<li>Matplotlib</li>
+<li>Numpy</li>
+<li>Data Cleaning and Machine Learning</li>
+<li>Image and Facial Recognition</li>
+`
+document.getElementById("border").appendChild(txt)
 
-var txt = document.createElement("dd")
-var p = document.createElement("p")
-p.id="small"
-p.innerHTML="SQL, Pandas, Matplotlib, Numpy"
-txt.appendChild(p)
-document.getElementById("listy").appendChild(txt)
-
-var txt = document.createElement("dt")
-var p = document.createElement("p")
-p.id="big"
-p.innerHTML="R"
-txt.appendChild(p)
-document.getElementById("listy").appendChild(txt)
-
-var txt = document.createElement("dd")
-var p = document.createElement("p")
-p.id="small"
-p.innerHTML="GGPlot2"
-txt.appendChild(p)
-document.getElementById("listy").appendChild(txt)
-
-var txt = document.createElement("dt")
-var p = document.createElement("p")
-p.id="big"
-p.innerHTML="JavaScript, HTML, CSS (web-building)"
-txt.appendChild(p)
-document.getElementById("listy").appendChild(txt)
+var txt = document.createElement("hr")
+document.getElementById("border").appendChild(txt)
 
 var txt = document.createElement("p")
-txt.innerHTML = "The Summer of 2019, I taught myslef to code in Python using codeacamy and Chuck Severence's Coursera University of Michigan Python for Everbody course. I learned how to apply the Python fundamentals to using SQL, parsing CSV, JSON, & XML, grabbing information from the web with Urllib, APIs, Beautiful Soup (parsing HTML), Element Tree (parsing xml), and Regular Expressions.\
-After taking these courses, I began building my own <a href='./projects/projects.html' target='_blank'>projects</a> such using the New York bus API and reading through a government complaint CSV file. Once I had mastered organizing data, I decided to learn JavaScript, HTML and CSS to create website that would highlight my work, including this one. For my final project, I took a Jeopardy JSON file, organized it into a multi-table SQL database, then selected the data into into a website that you\ can see <a href='./jeopardy/src/website/page/jeopardy.html' target='_blank'>here</a>."
-document.getElementById("writing").appendChild(txt)
+txt.id = "sk"
+txt.innerHTML = "R"
+document.getElementById("border").appendChild(txt)
+
+var txt = document.createElement("UL")
+txt.id = "listy2"
+txt.innerHTML = `
+<li>ggplot2</li>
+<li>Data Cleaning and Machine Learning</li>
+`
+document.getElementById("border").appendChild(txt)
+
+
+var txt = document.createElement("hr")
+document.getElementById("border").appendChild(txt)
 
 var txt = document.createElement("p")
-txt.innerHTML = "The summer of 2020, I worked for University of Chicago Economist and Researcher <a href=https://bfi.uchicago.edu/scholar/alex-weinberg/ target=_blank>Alex Weinberg</a>. My first task was to used Python and Pandas to clean U.S. census data, taking it from a dta file and converting it in to a csv file. Along with creating new organized columns, I also merged in government occupation and educational codes, which allowed me to drop falty and incomplete rows, adjust for inflation, and let go of data below 50% of the minimum wage. Next, using Matplotlib, Numpy and Pandas aggregates, I graphed the weighted mean wage, weighted variance, and other statistics over time. First, I graphed imitations of graphs from Autodorn's Economic Review in order to check that our data was accurate. Next, I moved on to graphs in R's ggplot (a better resource for statistical graphs); some of these graphs will be included in the published research paper."
-document.getElementById("writing").appendChild(txt)
+txt.id = "sk"
+txt.innerHTML = "HTML, CSS, JavaScript"
+document.getElementById("border").appendChild(txt)
+
+var txt = document.createElement("hr")
+document.getElementById("border").appendChild(txt)
+
+var txt = document.createElement("p")
+txt.id = "sk"
+txt.innerHTML = "Node.js (Bots (webhooks), website server)"
+document.getElementById("border").appendChild(txt)
+
+var txt = document.createElement("hr")
+document.getElementById("border").appendChild(txt)
+
+var txt = document.createElement("p")
+txt.id = "sk"
+txt.innerHTML = "React Native"
+document.getElementById("border").appendChild(txt)
+
+
+
+
+
 
 var txt = document.createElement("p")
 txt.id="code"
